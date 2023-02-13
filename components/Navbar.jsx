6da61d422960c131a4cd53767414f7fca9724baa
DevLaukey <devlaukeymwaura@gmail.com>
2023-02-13 21:28:33 +0300

    Completed page layout of landing

---
+++ components/Navbar.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white border-gray-400 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 mt-7">
+      <div className="container flex  items-center justify-around mx-auto flex-wrap md:flex-nowrap">
+        <a href="https://flowbite.com/" className="flex items-center m-2">
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="h-6 mr-3 sm:h-9"
+            alt="Flowbite Logo"
+          />
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            CRYTPOCERT{" "}
+          </span>
+        </a>
+       
+        <form className="w-3/4">
+          <label
+            for="default-search"
+            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+          >
+            Search
+          </label>
+          <div className="relative">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg
+                aria-hidden="true"
+                className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                ></path>
+              </svg>
+            </div>
+            <input
+              type="search"
+              id="default-search"
+              className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+              placeholder="Search Mockups, Logos..."
+              required
+            />
+            <button
+              type="submit"
+              className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import UploadForm from "../components/UploadForm";
 import contract from "../smartcontract/Certificate";
 import Web3  from "web3";
 import { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import CardGrid from "../components/CardGrid";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
@@ -303,39 +305,10 @@ export default function Home() {
 
   console.log(contract), console.log(web3), console.log(address);
   return (
-    <div className="container py-10 px-10 mx-0 min-w-full flex flex-col items-center">
-      {/* Intro and instructions of the application // File drop */}
-      {/* <FileUpload /> */}
-      <UploadForm />
-{/* 
-      The data must first be prepared: ie the Certificate images created and stored on IPFS and metadata of the data stord in tabular form
-
-      the data will be link to the Certificate image and some data describing the name, course, grade etc.
-
-      This prepare data will then be uploaded to the blockchain using CryptoCert. This will be a link of the prepared data that was uploaded to the IPFS and some data concerning those certs. ie the Year, school etc.
-
-      This will be pushed to the blockchain and later accessible.
-
-      For a student, to access their Certificate... They will pass thier contract ID + their unique school Identification number. This will be used to access the data on the blockchain.
-
-      The data will be retrieved and the data will be used to access the IPFS and retrieve the Certificate image. On the website ie: https://cryptocert.vercel.app/ the data will be displayed and the Certificate image will be displayed.
-
-
-      For this to work,
-      1. The data must be prepared and uploaded to IPFS
-      2. The data must be uploaded to the blockchain using CryptoCert
-      3. The data must be retrieved from the blockchain using CryptoCert
-      4. The data must be used to access the IPFS and retrieve the Certificate image
-      5. The data must be displayed on the website and the Certificate image must be displayed on the website
-
-      We will need a secure route where only the insitution can access the data. This will be used to upload the data to the blockchain. this will later added as a premium feature.
-
-      On landing to the cryptocert landing, the user will be able to see the list of all the certificates that have been uploaded to the blockchain. A search bar will be available to search for a particular certificate.
-
-      The user will be able to click on the certificate and view the certificate image and the data that was uploaded to the blockchain. */}
-
-
-    </div>
+    <div>
+      <Navbar />
+      <CardGrid/>
+   </div>
   );
 }
 
