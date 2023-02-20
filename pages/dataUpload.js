21d1e7b5ae0cfc89512e45de7a078d8fdc9bf573
DevLaukey <devlaukeymwaura@gmail.com>
2023-02-21 00:46:54 +0300

    Working on img to IPFS

--- pages/dataUpload.js
+++ pages/dataUpload.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState } from 'react'
 import { toast, ToastContainer } from "react-toastify";
 
@@ -31,23 +32,86 @@ const dataUpload = () => {
             };
         }
     }
+
+    const sendImgToIPFS = async () => { 
+        console.log("clicked");
+        imgsSrc.map(async (img) => {
+            if (img) {
+                try {
+                    const formData = new FormData();
+                    formData.append("file", img);
+                    await axios({
+                        method: "post",
+                        url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+                        data: img,
+                        headers: {
+                            'pinata_api_key': `${process.env.NEXT_PUBLIC_PINATA_API_KEY}`,
+                            'pinata_secret_api_key': `${process.env.NEXT_PUBLIC_PINATA_API_SECRET}`,
+                            "Content-Type": "multipart/form-data"
+                        },
+                    }).then((res) => {
+                        console.log(`ipfs://${res.data.IpfsHash}`);
+                        toast.success(`Image Uploaded Successfully to ipfs://${res.data.IpfsHash}`);
+                    }).catch((err) => { console.log(err) });
+                    //Take a look at your Pinata Pinned section, you will see a new file added to you list.   
+                } catch (error) {
+                    console.log("Error sending Image to IPFS: ")
+                    console.log(error)
+                }
+            }
+            else {
+                console.log("No Image Found");
+            }
+        });
+    }
+    const sendFileToIPFS = async (e) => {
+        e.preventDefault();
+        sendImgToIPFS();
+        // console.log("clicked");
+        // imgsSrc.map(async (fileImg) => {
+
+        //     if (fileImg) {
+        //         try {
+        //             const formData = new FormData();
+        //             formData.append("file", fileImg);
+        //             await axios({
+        //                 method: "post",
+        //                 url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+        //                 data: fileImg,
+        //                 headers: {
+        //                     'pinata_api_key': `${process.env.NEXT_PUBLIC_PINATA_API_KEY}`,
+        //                     'pinata_secret_api_key': `${process.env.NEXT_PUBLIC_PINATA_API_SECRET}`,
+        //                     "Content-Type": "multipart/form-data"
+        //                 },
+        //             }).then((res) => {
+        //                 console.log(`ipfs://${res.data.IpfsHash}`);
+        //                 toast.success(`File Uploaded Successfully to ipfs://${res.data.IpfsHash}`);
+        //             }).catch((err) => { console.log(err) });
+        //             //Take a look at your Pinata Pinned section, you will see a new file added to you list.   
+        //         } catch (error) {
+        //             console.log("Error sending File to IPFS: ")
+        //             console.log(error)
+        //         }
+        //     }
+        // });
+    }
+
+    const clearFrom = () => { }
     console.log(imgsSrc, imgsSrc.length, fileSrc);
     return (
-        <div>
-            <section class="container mx-auto max-w-screen-lg h-full">
+        <div className="flex flex-col">
+            <section class="container mx-auto max-w-screen-lg h-full flex flex-wrap">
                 <article aria-label="File Upload Modal" class="relative h-full flex flex-col bg-white shadow-xl rounded-md" >
 
                     <section class="h-full overflow-auto p-8 w-full flex flex-col">
                         <div class="w-full flex justify-between">
 
-                        <div class="border-dashed border-2 w-full border-gray-400 py-12 flex flex-col justify-center items-center">
-                            <p class="mb-3 font-semibold text-gray-900 flex flex-wrap justify-center">
-                                <span>Choose Cert</span>&nbsp;<span>Images</span>
-                            </p>
-                            <input onChange={onImgChange} type="file" name="file" multiple />
-                        </div>
-
-                      
+                            <div class="border-dashed border-2 w-full border-gray-400 py-12 flex flex-col justify-center items-center">
+                                <p class="mb-3 font-semibold text-gray-900 flex flex-wrap justify-center">
+                                    <span>Choose Cert</span>&nbsp;<span>Images</span>
+                                </p>
+                                <input onChange={onImgChange} type="file" name="file" multiple />
+                            </div>
                             <div class="border-dashed border-2 w-full border-gray-400 py-12 flex flex-col justify-center items-center">
                                 <p class="mb-3 font-semibold text-gray-900 flex flex-wrap justify-center">
                                     <span>Choose Cert</span>&nbsp;<span>Excel Sheet as CSV file</span>
@@ -55,15 +119,9 @@ const dataUpload = () => {
                                 <input onChange={onCertUpload} type="file" name="file" multiple />
                             </div>
                         </div>
-
-                            <h1 class="pt-8 pb-3 font-semibold sm:text-lg text-gray-900">
-                                To Upload
-                            </h1>
-
-
-
-                     
-
+                        <h1 class="pt-8 pb-3 font-semibold sm:text-lg text-gray-900">
+                            To Upload
+                        </h1>
                         <ul id="gallery" class="flex flex-1 flex-wrap -m-1">
                             {imgsSrc.length !== 0 ?
 
@@ -76,32 +134,24 @@ const dataUpload = () => {
                                         <img class="mx-auto w-32" src="https://user-images.githubusercontent.com/507615/54591670-ac0a0180-4a65-11e9-846c-e55ffce0fe7b.png" alt="no data" />
                                         <span class="text-small text-gray-500">No files selected</span>
                                     </li>)}
-
                         </ul>
                     </section>
-
-                 
                 </article>
             </section>
             <section class="container mx-auto max-w-screen-lg h-full">
                 <article aria-label="File Upload Modal" class="relative h-full flex flex-col bg-white shadow-xl rounded-md" >
-
-
                     <footer class="flex justify-end px-8 pb-8 pt-4">
-                        <button id="submit" class="rounded-sm px-3 py-1 bg-blue-700 hover:bg-blue-500 text-white focus:shadow-outline focus:outline-none">
+                        <button id="submit" class="rounded-sm px-3 py-1 bg-blue-700 hover:bg-blue-500 text-white focus:shadow-outline focus:outline-none" onClick={sendFileToIPFS}>
                             Upload now
                         </button>
-                        <button id="cancel" class="ml-3 rounded-sm px-3 py-1 hover:bg-gray-300 focus:shadow-outline focus:outline-none">
+                        <button onClick={clearFrom} id="cancel" class="ml-3 rounded-sm px-3 py-1 hover:bg-gray-300 focus:shadow-outline focus:outline-none">
                             Cancel
                         </button>
                     </footer>
                 </article>
             </section>
         </div>)
-    {/* <input onChange={onChange} type="file" name="file" multiple />
-{imgsSrc.map((link) => (
-    <img src={link} />
-))} */}
+
 }
 
 export default dataUpload
